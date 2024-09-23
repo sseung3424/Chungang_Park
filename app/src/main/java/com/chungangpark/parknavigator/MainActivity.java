e5bf847149d32c2e130b5849c37797b17fad3f5a
User <kogeon6@naver.com>
2024-09-23 12:44:58 +0900

    선형 점자블록 추가

--- app/src/main/java/com/chungangpark/parknavigator/MainActivity.java
+++ app/src/main/java/com/chungangpark/parknavigator/MainActivity.java
@@ -1,5 +1,12 @@
 package com.chungangpark.parknavigator;
+// 점형 점자블록 추가
+import com.naver.maps.map.overlay.Marker;
+// 선형 점자 블록 추가
+import com.naver.maps.map.overlay.PolylineOverlay;
+//latlng 클래스 임포트
+import com.naver.maps.geometry.LatLng;
 
+import java.util.Arrays;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +16,7 @@ import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.UiSettings;
+
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -41,5 +49,39 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // 위치 추적 모드를 None으로 설정 (원하는 경우 Follow로 변경 가능)
         naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+        // 선형 점자블록 추가
+        addLinearBrailleBlock(naverMap, new LatLng(37.52719621, 126.93243238), new LatLng(37.52746540, 126.93278579));
+        addLinearBrailleBlock(naverMap, new LatLng(37.52746078, 126.93283010), new LatLng(37.52677942, 126.93364775));
+        addLinearBrailleBlock(naverMap, new LatLng(37.52674941, 126.93364060), new LatLng(37.52672805, 126.93358961));
+        addLinearBrailleBlock(naverMap, new LatLng(37.52672805, 126.93358961), new LatLng(37.52659294, 126.93330747));
+        addLinearBrailleBlock(naverMap, new LatLng(37.52656957, 126.93325711), new LatLng(37.52656118, 126.93323845));
+        addLinearBrailleBlock(naverMap, new LatLng(37.52657473, 126.93330811), new LatLng(37.52656231, 126.93341181));
+        addLinearBrailleBlock(naverMap, new LatLng(37.52656231, 126.93341181), new LatLng(37.52655814, 126.93351839));
+        addLinearBrailleBlock(naverMap, new LatLng(37.52655814, 126.93351839), new LatLng(37.52654010, 126.93361643));
+        addLinearBrailleBlock(naverMap, new LatLng(37.52654010, 126.93361643), new LatLng(37.52650548, 126.93375344));
+        addLinearBrailleBlock(naverMap, new LatLng(37.52650548, 126.93375344), new LatLng(37.52644432, 126.93393378));
+
+        // 점형 점자블록 추가
+        addDotBrailleBlock(naverMap, new LatLng(37.5666102, 126.9783881), new LatLng(37.5668202, 126.9786881));
     }
+
+    // 선형 점자블록 추가 함수
+    private void addLinearBrailleBlock(NaverMap naverMap, LatLng startPoint, LatLng endPoint) {
+        PolylineOverlay polyline = new PolylineOverlay();
+        polyline.setCoords(Arrays.asList(startPoint, endPoint));
+        polyline.setColor(0xFFFFA500); // 주황색 선
+        polyline.setMap(naverMap);
+    }
+
+    // 점형 점자블록 추가 함수
+    private void addDotBrailleBlock(NaverMap naverMap, LatLng startPoint, LatLng endPoint) {
+        PolylineOverlay polyline = new PolylineOverlay();
+        polyline.setCoords(Arrays.asList(startPoint, endPoint));
+        polyline.setColor(0xFFFF00A5); // 핑크색 선
+        polyline.setMap(naverMap);
+    }
+
+
+
+
 }
