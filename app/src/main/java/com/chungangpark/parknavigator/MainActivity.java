71d915257eac012f3fa1c17d584dc1a5283efe1e
csh3424 <csh3424@icloud.com>
2024-09-24 23:44:13 +0900

    위치 추적 기능 추가

--- app/src/main/java/com/chungangpark/parknavigator/MainActivity.java
+++ app/src/main/java/com/chungangpark/parknavigator/MainActivity.java
@@ -1,28 +1,56 @@
 package com.chungangpark.parknavigator;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.LocationTrackingMode;
-import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.NaverMapOptions;
+import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.LocationOverlay;
+import com.naver.maps.map.util.FusedLocationSource;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
 
     private NaverMap naverMap;
+    private FusedLocationSource locationSource;
+    private Animator animator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ActionBar 설정 (위치 추적 모드를 위한)
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+
+        // FusedLocationSource 설정 (위치 권한 요청)
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+
         // MapFragment 가져오기
         MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
         if (mapFragment == null) {
-            mapFragment = MapFragment.newInstance(new NaverMapOptions());
+            mapFragment = MapFragment.newInstance(new NaverMapOptions().locationButtonEnabled(true));
             getSupportFragmentManager().beginTransaction().add(R.id.map_fragment, mapFragment).commit();
         }
 
@@ -32,14 +60,71 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-        // 네이버 지도 객체 가져오기
+        // 네이버 지도 객체 설정
         this.naverMap = naverMap;
 
-        // 지도 UI 설정 (줌 버튼 등)
+        // 지도 UI 설정 (줌 버튼)
         UiSettings uiSettings = naverMap.getUiSettings();
-        uiSettings.setZoomControlEnabled(true); // 줌 컨트롤 설정
+        uiSettings.setZoomControlEnabled(true);
 
-        // 위치 추적 모드를 None으로 설정 (원하는 경우 Follow로 변경 가능)
-        naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+        // 위치 오버레이 설정
+        LocationOverlay locationOverlay = naverMap.getLocationOverlay();
+        locationOverlay.setVisible(true);
+        locationOverlay.setSubIcon(LocationOverlay.DEFAULT_SUB_ICON_ARROW);
+        locationOverlay.setCircleOutlineWidth(0); // 원 테두리 제거
+
+        // 위치 추적 설정
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Face);
+    }
+
+    // 원 애니메이션
+    private void animateCircle(@NonNull LocationOverlay locationOverlay) {
+        if (animator != null) {
+            animator.cancel();
+        }
+
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ObjectAnimator radiusAnimator = ObjectAnimator.ofInt(locationOverlay, "circleRadius",
+                0, getResources().getDimensionPixelSize(R.dimen.location_overlay_circle_raduis));
+        radiusAnimator.setRepeatCount(2);
+
+        ObjectAnimator colorAnimator = ObjectAnimator.ofInt(locationOverlay, "circleColor",
+                Color.argb(127, 148, 186, 250), Color.argb(0, 148, 186, 250));
+        colorAnimator.setEvaluator(new ArgbEvaluator());
+        colorAnimator.setRepeatCount(2);
+
+        animatorSet.setDuration(1000);
+        animatorSet.playTogether(radiusAnimator, colorAnimator);
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                locationOverlay.setCircleRadius(0);
+            }
+        });
+        animatorSet.start();
+
+        animator = animatorSet;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) {
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
