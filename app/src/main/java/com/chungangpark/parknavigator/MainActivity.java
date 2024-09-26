7df3349fb606eb325718ae1878ea2e35d44e462c
csh3424 <csh3424@icloud.com>
2024-09-27 01:59:35 +0900

    특정 장소 저장

--- app/src/main/java/com/chungangpark/parknavigator/MainActivity.java
+++ app/src/main/java/com/chungangpark/parknavigator/MainActivity.java
@@ -8,11 +8,8 @@ import android.animation.ObjectAnimator;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,6 +21,13 @@ import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.LocationOverlay;
 import com.naver.maps.map.util.FusedLocationSource;
+
+import com.naver.maps.geometry.LatLng;   // LatLng 클래스 임포트
+import com.naver.maps.map.overlay.Marker; // Marker 클래스 임포트
+import com.naver.maps.map.overlay.OverlayImage; // Marker 아이콘 관련
+
+import java.util.List;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
@@ -58,7 +62,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
-    @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         // 네이버 지도 객체 설정
         this.naverMap = naverMap;
@@ -76,7 +79,55 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // 위치 추적 설정
         naverMap.setLocationSource(locationSource);
         naverMap.setLocationTrackingMode(LocationTrackingMode.Face);
+
+        // toilet_list
+        List<LatLng> toilet = Arrays.asList(
+                // 아래는 예시 위치 -> 화장실 좌표 추가해야함!!!!!
+                new LatLng(37.5666102, 126.9783881), // 서울시청
+                new LatLng(37.570041, 126.982794),   // 경복궁
+                new LatLng(37.551229, 126.988205)    // 남산타워
+        );
+        // information_list
+        List<LatLng> information = Arrays.asList(
+                // 아래는 예시 위치 -> 안내소 좌표 추가해야함!!!!!
+                new LatLng(37.5666102, 126.9783881), // 서울시청
+                new LatLng(37.570041, 126.982794),   // 경복궁
+                new LatLng(37.551229, 126.988205)    // 남산타워
+        );
+        // store_list
+        List<LatLng> store = Arrays.asList(
+                // 아래는 예시 위치 -> 매점 좌표 추가해야함!!!!!
+                new LatLng(37.5666102, 126.9783881), // 서울시청
+                new LatLng(37.570041, 126.982794),   // 경복궁
+                new LatLng(37.551229, 126.988205)    // 남산타워
+        );
+        // 아래에 다른 위치 추가 필요
+
+        // toilet_marker
+        for (LatLng place : toilet) {
+            Marker marker = new Marker();
+            marker.setPosition(place); // 심볼 위치 설정
+            marker.setMap(naverMap);   // 지도에 마커 추가
+            marker.setIcon(OverlayImage.fromResource(R.drawable.custom_marker)); // 커스텀 마커 이미지 설정 가능
+        }
+        // information_marker
+        for (LatLng place : information) {
+            Marker marker = new Marker();
+            marker.setPosition(place); // 심볼 위치 설정
+            marker.setMap(naverMap);   // 지도에 마커 추가
+            marker.setIcon(OverlayImage.fromResource(R.drawable.custom_marker)); // 커스텀 마커 이미지 설정 가능
+        }
+        // store_marker
+        for (LatLng place : store) {
+            Marker marker = new Marker();
+            marker.setPosition(place); // 심볼 위치 설정
+            marker.setMap(naverMap);   // 지도에 마커 추가
+            marker.setIcon(OverlayImage.fromResource(R.drawable.custom_marker)); // 커스텀 마커 이미지 설정 가능
+        }
+
+        // Marker는 심볼처럼 기본적으로 지도에 떠 있기 때문에 추가 설정 없이도 지도에 표시됨.
     }
+    // 아래에 마커 추가 필요
 
     // 원 애니메이션
     private void animateCircle(@NonNull LocationOverlay locationOverlay) {
